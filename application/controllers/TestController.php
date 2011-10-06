6bf78829003c8736c0de4b69ca445519b743ab84
Mark Swanson <markisacat@gmail.com>
2011-10-05 21:21:42 -0400

    working on tress

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -3,8 +3,14 @@
 class TestController extends Custom_Zend_Controller_Action
 {
 
-    public function indexAction() {
-		
+    public function indexAction() {    	
+    	$treeMapper = new Custom_Tree('books.xml');
+    	
+    	$books = $treeMapper->getSimpleXML();
+    	
+  		//$books = $treeMapper->saveSimpleXML();
+    	
+    	Zend_Debug::dump($books);
     }
 
 	public function javascriptAction() {}
