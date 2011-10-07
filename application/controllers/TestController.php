0a39f22c61124321ea9b58d3ea2df327b59b7671
Mark Swanson <markisacat@gmail.com>
2011-10-07 19:02:31 -0400

    testing

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -3,14 +3,23 @@
 class TestController extends Custom_Zend_Controller_Action
 {
 
-    public function indexAction() {    	
-    	$treeMapper = new Custom_Tree('books.xml');
+    public function indexAction() {
+		$booksTree = new Custom_Tree(DIR_TREES.'/books.xml');
+    	//$xml = $booksTree->getXML();
+    	//echo $xml->childNodes->item(0);
     	
-    	$books = $treeMapper->getSimpleXML();
+		//$booksTree->addValue(array('blue', 'leather', 'heel3'));
+    	$booksTree->removeValue(array('orange', 'leather', 'heel1'));
+ 
+    	$booksTree->echoXML();
+    	$booksTree->saveXML();
+    	
+    	
+    	//$books = $treeMapper->getSimpleXML();
     	
   		//$books = $treeMapper->saveSimpleXML();
     	
-    	Zend_Debug::dump($books);
+    	//Zend_Debug::dump($books);
     }
 
 	public function javascriptAction() {}
